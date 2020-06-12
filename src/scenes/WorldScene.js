e4c2efb387e9abb3c5c884eae5b9386cb7b49afb
Da1man <chaos4ever@mail.ru>
2020-06-12 23:48:33 +0300

    init project structure

---
+++ src/scenes/WorldScene.js
@@ -0,0 +1,18 @@
+import Phaser from 'phaser';
+
+export default class WorldScene extends Phaser.Scene {
+  constructor() {
+    super('WorldScene')
+  }
+
+  init() {
+  }
+
+  preload() {
+  }
+
+  create() {
+    console.log('WorldScene created')
+  }
+
+}
